1c1b5d85d7302dd9cdb08af7989665733a56e614
Rodrigo Toledo <rodrigo.toledo04@hotmail.com>
2023-04-03 11:03:19 -0300

    inicializacao

---
+++ ProjetoMiniBlog/miniblog_/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBrYB21_iNiSx36hG_T5Mkkls7qNR_52yc",
+  authDomain: "portfolio-adbc6.firebaseapp.com",
+  projectId: "portfolio-adbc6",
+  storageBucket: "portfolio-adbc6.appspot.com",
+  messagingSenderId: "734217179071",
+  appId: "1:734217179071:web:7be61a45f97b0960699b0a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
